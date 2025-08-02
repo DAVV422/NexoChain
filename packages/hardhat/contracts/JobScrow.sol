ef82b8512bc1925a1ac19958e2e67fbd4a2ecced
Job Natanael Contreras Garzon <143237173+Job-Natanael-Contreras-Garzon@users.noreply.github.com>
2025-08-02 11:26:01 -0400

    new update

--- packages/hardhat/contracts/JobScrow.sol
+++ packages/hardhat/contracts/JobScrow.sol
@@ -21,7 +21,7 @@ contract JobEscrow {
 
     // Mapeo de un ID de trabajo a su estructura de datos.
     mapping(uint256 => Job) public jobs;
-    uint256 public nextJobId = 0;
+    uint256 public nextJobId = 1;
 
     // --- ROLES DEL CONTRATO ---
     address public immutable arbiter; // El árbitro que resuelve disputas.
